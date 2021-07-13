e2cd8d26284bfc01c52b42667e4120846cd827e8
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-12 22:45:00 -0300

    Aula 'Interface de Detalhes dos carros' (C3-M1-SM1)

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {
+  Container,
+  Header,
+} from './styles'
+
+import { BackButton } from '../../components/BackButton'
+
+export function CarDetails() {
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+    </Container>
+  )
+}
