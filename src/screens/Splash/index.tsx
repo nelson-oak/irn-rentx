84a1424611a771ecc770b38965fd3db6777c57a0
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-15 20:17:33 -0300

    Aula 'Transições' (C3-M2-SM1)

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,23 +1,36 @@
 import React from 'react'
-import { StatusBar, Button, StyleSheet } from 'react-native'
-import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
+import { StatusBar, Button, StyleSheet, Dimensions } from 'react-native'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  Easing
+} from 'react-native-reanimated'
 
 import {
   Container,
 } from './styles'
+
+const WIDTH = Dimensions.get('window').width
 
 export function Splash() {
   const animation = useSharedValue(0)
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
-        { translateX: animation.value },
+        {
+          translateX: withTiming(animation.value, {
+            duration: 3000,
+            easing: Easing.bezier(0,1.95,1,-1.13)
+          })
+        },
       ]
     }
   })
 
   function handleAnimatePosition() {
-    animation.value = Math.random() * 350
+    // animation.value = Math.random() * (WIDTH - 100)
+    animation.value = animation.value ? 0 : WIDTH - 100
   }
 
   return (
