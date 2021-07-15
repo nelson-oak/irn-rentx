1e5ee9c897074a1ca2759a4643064b8bd82f4909
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-15 18:28:27 -0300

    Aula 'Iniciando o Slide de Imagens' (C3-M2-SM1)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -65,7 +65,7 @@ export function Home() {
           <Logo width={RFValue(108)} height={RFValue(12)} />
 
           <TotalCars>
-            Total de 12 carros
+            Total de {cars.length} carros
           </TotalCars>
         </HeaderContent>
       </Header>
