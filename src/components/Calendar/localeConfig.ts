b02bb4ca65803419f7b732ac951849e29aab8513
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-14 21:57:22 -0300

    Aula 'Intervalos no Calendário' (C3-M1-SM2)

---
+++ src/components/Calendar/localeConfig.ts
@@ -0,0 +1,21 @@
+export const ptBR = {
+  monthNames: [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril',
+    'Maio', 'Junho', 'Julho', 'Agosto',
+    'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ],
+  monthNamesShort: [
+    'JAN', 'FEV', 'MAR', 'ABR',
+    'MAI', 'JUN', 'JUL', 'AGO',
+    'SET', 'OUT', 'NOV' , 'DEZ'
+  ],
+  dayNames: [
+    'Domingo', 'Segunda', 'Terça', 'Quarta',
+    'Quinta', 'Sexta', 'Sábado'
+  ],
+  dayNamesShort: [
+    'DOM', 'SEG', 'TER', 'QUA',
+    'QUI', 'SEX', 'SAB'
+  ],
+  today: 'Hoje'
+}
