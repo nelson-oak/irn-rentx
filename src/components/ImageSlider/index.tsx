7beecebd850d41951fabc2671f8354d84bba37ab
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-15 18:52:51 -0300

    Aula 'Obtendo a imagem em foco' (C3-M2-SM1)

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FlatList } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { FlatList, ViewToken } from 'react-native'
 
 import {
   Container,
@@ -13,13 +13,28 @@ interface IImageSliderProps {
   imagesUrl: string[]
 }
 
+interface IChangeImageProps {
+  viewableItems: ViewToken[]
+  changed: ViewToken[]
+}
+
 export function ImageSlider({ imagesUrl }: IImageSliderProps) {
+  const [imageIndex, setImageIndex] = useState(0)
+
+  const indexChanged = useRef((info: IChangeImageProps) => {
+    const index = info.viewableItems[0].index!
+    setImageIndex(index)
+  })
+
   return (
     <Container>
       <ImageIndexes>
         {
           imagesUrl.map((_, index) => (
-            <ImageIndex key={String(index)} active={true} />
+            <ImageIndex
+              key={String(index)}
+              active={index === imageIndex}
+            />
           ))
         }
       </ImageIndexes>
@@ -37,6 +52,7 @@ export function ImageSlider({ imagesUrl }: IImageSliderProps) {
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
       />
     </Container>
   )
