d77e7476a2f4a91651b0e2655d4201fce28c627d
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-15 00:46:11 -0300

    Aula 'Finalizando o botão' (C3-M1-SM2)

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -53,6 +53,7 @@ interface IRentalPeriod {
 }
 
 export function SchedulingDetails() {
+  const [loading, setLoading] = useState(false)
   const [rentalPeriod, setRentalPeriod] = useState<IRentalPeriod>({} as IRentalPeriod)
   
   const theme = useTheme()
@@ -63,6 +64,8 @@ export function SchedulingDetails() {
   const rentTotal = dates.length * car.rent.price
 
   async function handleConfirmRental() {
+    setLoading(true)
+
     const schedulesByCar = await api.get(`/schedules_bycars/${car.id}`)
 
     const unavailable_dates = [
@@ -83,7 +86,10 @@ export function SchedulingDetails() {
         unavailable_dates
       })
       .then(() => navigation.navigate('SchedulingComplete'))
-      .catch(() => Alert.alert('Não foi possível confirmar o agendamento'))
+      .catch(() => {
+        Alert.alert('Não foi possível confirmar o agendamento')
+        setLoading(false)
+      })
   }
 
   function handleGoBack() {
@@ -170,6 +176,8 @@ export function SchedulingDetails() {
           title="Alugar agora"
           color={theme.colors.success}
           onPress={handleConfirmRental}
+          enabled={!loading}
+          loading={loading}
         />
       </Footer>
     </Container>
