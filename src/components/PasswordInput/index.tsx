3d4bf3ff047ebe89f61abfc3745706e5468692a3
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-22 14:02:09 -0300

    Aula 'Acessibilidade em Inputs' (C4-M1-SM2)

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -12,31 +12,47 @@ import { BorderlessButton } from 'react-native-gesture-handler'
 
 interface IPasswordProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>['name']
+  value?: string
 }
 
 export function PasswordInput({
   iconName,
+  value,
   ...rest
 }: IPasswordProps) {
   const [isPasswordVisible, setIsPasswordVisible] = useState(false)
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+
   const theme = useTheme()
+
+  function handleInputFocus() {
+    setIsFocused(true)
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false)
+    setIsFilled(!!value)
+  }
 
   function togglePasswordVisibility() {
     setIsPasswordVisible(!isPasswordVisible)
   }
 
   return (
-    <Container>
+    <Container isFocused={isFocused}>
       <IconContainer>
         <Feather
           name={iconName}
           size={24}
-          color={theme.colors.text_details}
+          color={isFocused || isFilled ? theme.colors. main : theme.colors.text_details}
         />
       </IconContainer>
 
       <InputText
         secureTextEntry={!isPasswordVisible}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         {...rest}
       />
 
