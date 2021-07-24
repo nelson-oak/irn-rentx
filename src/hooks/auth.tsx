d3cadbf64341e967d01ded7b9fbdf6715713b981
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-07-24 14:10:24 -0300

    Aula 'Autenticação' (C4-M1-SM3)

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,65 @@
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode
+} from 'react'
+
+import { api } from '../services/api'
+
+interface IUser {
+  id: string
+  name: string
+  email: string
+  driver_license: string
+  avatar: string
+}
+
+interface IAuthState {
+  token: string
+  user: IUser
+}
+
+interface  ISignInCredentials {
+  email: string
+  password: string
+}
+
+interface IAuthContextData {
+  user: IUser
+  signIn: (credentials: ISignInCredentials) => Promise<void>
+}
+
+interface IAuthProviderProps {
+  children: ReactNode
+}
+
+const AuthContext = createContext<IAuthContextData>({} as IAuthContextData)
+
+export function AuthProvider({ children }: IAuthProviderProps) {
+  const [data, setData] = useState<IAuthState>({} as IAuthState)
+
+  async function signIn({ email, password }: ISignInCredentials) {
+    const response = await api.post('/sessions', {
+      email,
+      password
+    })
+
+    const { token, user } = response.data
+    setData({ token, user })
+  }
+
+  return (
+    <AuthContext.Provider value={{
+      user: data.user,
+      signIn
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth(): IAuthContextData {
+  const context = useContext(AuthContext)
+  return context
+}
---
+++ src/hooks/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+import { AuthProvider } from './auth'
+
+interface IAppProviderProps {
+  children: ReactNode
+}
+
+export function AppProvider({ children }: IAppProviderProps) {
+  return (
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  )
+}
